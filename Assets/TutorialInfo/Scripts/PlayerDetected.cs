4863f149f174f4f73594abb8b41c806d93b23ea9
lucivalente09 <lucivalente09@gmail.com>
2025-12-29 18:29:38 -0300

    Paper_Exam
    Papel de prueba listo

--- Assets/TutorialInfo/Scripts/PlayerDetected.cs
+++ Assets/TutorialInfo/Scripts/PlayerDetected.cs
@@ -22,8 +22,9 @@ public class PlayerDetected : MonoBehaviour
     bool IsLighter = false;
     protected bool IsKey = false;
     bool IsHammer = false;
+    bool IsPaper = false;
     [SerializeField] GameObject[] Planks;
-    [SerializeField]int int_Planks = 4;
+    [SerializeField] int int_Planks = 4;
 
 
     [SerializeField] GameObject[] ObjectsEquipment;
@@ -49,7 +50,7 @@ public class PlayerDetected : MonoBehaviour
 
         set
         {
-            
+
             Transparency = Mathf.Clamp(value, 0, 1);
         }
     }
@@ -61,13 +62,13 @@ public class PlayerDetected : MonoBehaviour
     private void Start()
     {
         Transpayency_Int = 1;
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
         RaycastHit hit = new RaycastHit();
 
 
@@ -100,7 +101,7 @@ public class PlayerDetected : MonoBehaviour
         // Rayo que detecta el layer "Object"
         if (Physics.Raycast(ray, out hit, 16, layers[2]))
         {
-           
+
             Debug.Log("Hit object: " + hit.transform.name);
             // Si se detecta una llave, se desactiva el objeto en la escena, se activa la variable IsKey y se muestra el objeto en la mano del jugador
             if (hit.collider.CompareTag("Key") && Input.GetMouseButtonDown(0))
@@ -115,7 +116,7 @@ public class PlayerDetected : MonoBehaviour
                     ObjectsEquipment[0].SetActive(true);
                     ObjectsEquipment[0].transform.position = RightHand.transform.position;
                     ObjectsEquipment[0].transform.SetParent(RightHand.transform, true);
-                   
+
 
                 }
 
@@ -130,7 +131,7 @@ public class PlayerDetected : MonoBehaviour
                     hit.collider.transform.position = RightHand.transform.position;
 
                     hit.collider.transform.SetParent(RightHand.transform, true);
-                    
+
                 }
 
 
@@ -146,7 +147,7 @@ public class PlayerDetected : MonoBehaviour
                     ObjectsEquipment[2].SetActive(true);
                     ObjectsEquipment[2].transform.position = RightHand.transform.position;
                     ObjectsEquipment[2].transform.SetParent(RightHand.transform, true);
-               
+
 
                 }
 
@@ -166,12 +167,12 @@ public class PlayerDetected : MonoBehaviour
 
                     if (RightHand.transform.childCount >= 1)
                     {
-                         GameObject ChildHand = RightHand.gameObject.transform.transform.GetChild(0).gameObject;
+                        GameObject ChildHand = RightHand.gameObject.transform.transform.GetChild(0).gameObject;
                         Debug.Log(RightHand.gameObject.transform.transform.GetChild(0).gameObject);
                         ChildHand.transform.position = Exam_Candle.transform.position;
                         ChildHand.transform.SetParent(Exam_Candle.transform, true);
                         ChildHand.tag = "Untagged";
-                       
+
                     }
 
                 }
@@ -196,94 +197,113 @@ public class PlayerDetected : MonoBehaviour
                 ObjectsEquipment[3].SetActive(true);
                 ObjectsEquipment[3].transform.position = RightHand.transform.position;
                 ObjectsEquipment[3].transform.SetParent(RightHand.transform, true);
-               
+
                 PlanksFall();
             }
 
-        }
 
-
-
-        Debug.Log(Transparency);
-        // Rayo que detecta el layer "Wall" para cambiar el layer del rayo que detecta los objetos interactuables a "Object"
-        if (Physics.Raycast(ray, out hit, 16, layers[3]))
-        {
-
-            layers[2] = LayerMask.GetMask("Nothing");
-        }
-        else if (!Physics.Raycast(ray, out hit, 16, layers[3]))
-        {
-            layers[2] = LayerMask.GetMask("Object");
-
-        }
-
-
-        if (Physics.Raycast(ray, out hit, 16, layers[4]) && IsKey)
-        {
-           
-            Pos_OpenDrawer = new Vector3(Pos_OpenDrawer.x, hit.transform.localPosition.y, Pos_OpenDrawer.z);
-            if (HitBox_Lock.TryGetComponent<Rigidbody>(out _))
+            if (hit.collider.CompareTag("Paper") && Input.GetMouseButtonDown(0))
             {
- 
-                Destroy(ObjectsEquipment[0]);
+                Debug.Log("Paper_Detected");
+                hit.collider.gameObject.SetActive(false);
+                IsPaper = true;
+                ObjectsEquipment[4].SetActive(true);
+                ObjectsEquipment[4].transform.position = RightHand.transform.position;
+                ObjectsEquipment[4].transform.SetParent(RightHand.transform, true);
 
-                if (Input.GetMouseButton(0))
+            }
+
+            if (hit.collider.CompareTag("Exam_Paper") && IsPaper)
+            {
+                if (!Tools_Active)
                 {
-                    hit.transform.localPosition = Vector3.MoveTowards(hit.transform.localPosition, Pos_OpenDrawer, SpeedDrawer * Time.deltaTime);
-                    Debug.Log("AAAA");
+
                 }
             }
-        }
-
-        Locking(HitBox_Lock); // Pass the required argument to the Locking method  
-    }
 
 
 
-    void Locking(GameObject HitBox_Lock)
-    {
-        if (IsKey)
-        {
-            HitBox_Lock.layer = LayerMask.NameToLayer("Fall_Object");
-
-
-        }
-    }
-
-    void PlanksFall()
-    {
-        if (IsHammer && int_Planks > 0)
-        {
-            foreach (GameObject plank in Planks)
+            Debug.Log(Transparency);
+            // Rayo que detecta el layer "Wall" para cambiar el layer del rayo que detecta los objetos interactuables a "Object"
+            if (Physics.Raycast(ray, out hit, 16, layers[3]))
             {
-                plank.layer = LayerMask.NameToLayer("Fall_Object");
-                int_Planks--;
+
+                layers[2] = LayerMask.GetMask("Nothing");
             }
- 
+            else if (!Physics.Raycast(ray, out hit, 16, layers[3]))
+            {
+                layers[2] = LayerMask.GetMask("Object");
+
+            }
+
+
+            if (Physics.Raycast(ray, out hit, 16, layers[4]) && IsKey)
+            {
+
+                Pos_OpenDrawer = new Vector3(Pos_OpenDrawer.x, hit.transform.localPosition.y, Pos_OpenDrawer.z);
+                if (HitBox_Lock.TryGetComponent<Rigidbody>(out _))
+                {
+
+                    Destroy(ObjectsEquipment[0]);
+
+                    if (Input.GetMouseButton(0))
+                    {
+                        hit.transform.localPosition = Vector3.MoveTowards(hit.transform.localPosition, Pos_OpenDrawer, SpeedDrawer * Time.deltaTime);
+                        Debug.Log("AAAA");
+                    }
+                }
+            }
+
+            Locking(HitBox_Lock); // Pass the required argument to the Locking method  
         }
-    }
 
 
 
-    IEnumerator TransparencyLoad()
-    {
-        Material Ice = IceObject.GetComponent<Renderer>().material;
-        while (Transpayency_Int >= 0)
+        void Locking(GameObject HitBox_Lock)
         {
-            Transpayency_Int -= 0.1f;
+            if (IsKey)
+            {
+                HitBox_Lock.layer = LayerMask.NameToLayer("Fall_Object");
 
 
-            Ice.SetFloat("_Transparent", Transpayency_Int);
-            yield return new WaitForSeconds(0.1f);
-            Ice.SetColor("_Color_Back", Color.black);
-            yield return null;
+            }
         }
 
-        if (Transpayency_Int == 0)
+        void PlanksFall()
         {
-            Destroy(IceObject);
+            if (IsHammer && int_Planks > 0)
+            {
+                foreach (GameObject plank in Planks)
+                {
+                    plank.layer = LayerMask.NameToLayer("Fall_Object");
+                    int_Planks--;
+                }
+
+            }
         }
-    
-        
+
+
+
+        IEnumerator TransparencyLoad()
+        {
+            Material Ice = IceObject.GetComponent<Renderer>().material;
+            while (Transpayency_Int >= 0)
+            {
+                Transpayency_Int -= 0.1f;
+
+
+                Ice.SetFloat("_Transparent", Transpayency_Int);
+                yield return new WaitForSeconds(0.1f);
+                Ice.SetColor("_Color_Back", Color.black);
+                yield return null;
+            }
+
+            if (Transpayency_Int == 0)
+            {
+                Destroy(IceObject);
+            }
+
+
+        }
     }
 }
